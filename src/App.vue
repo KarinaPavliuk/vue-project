87ee6a0596726d2ed4d241edfefc48eaa3d87fd3
Karina Pavliuk <kpavliuk8@gmail.com>
2024-02-09 23:36:26 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <button type="button" @click="component = 'Reg'">Reg</button>
+    <button type="button" @click="component = 'Aut'">Aut</button>
+    <keep-alive><component :is="component"></component></keep-alive>
+  </div>
+</template>
+
+<script>
+// import List from "./components/List.vue"; // імпортуєм компонент локально
+import Aut from "./components/Aut.vue";
+import Reg from "./components/Reg.vue";
+
+export default {
+  components: {
+    Aut,
+    Reg,
+  },
+  data() {
+    return {
+      component: "Aut",
+    };
+  },
+};
+</script>
+
+<style></style>
